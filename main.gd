4bc4632fd87a00f72dba0447a16675794bc58267
yonshin <yonshin@student.42seoul.kr>
2023-11-02 16:17:40 +0900

    refactor: remove unused func

--- main.gd
+++ main.gd
@@ -6,6 +6,3 @@ func _enter_tree():
 func _ready():
 	get_node("map").connect("broken_ore", get_node("player").broken_ore)
 	get_node("hud").emit(get_node("player"))
-
-func _print(value):
-	print(value)
